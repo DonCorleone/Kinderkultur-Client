e17abb0e371032293ab1419c80948a8c246e39f1
Developer <dev@Administrators-MacBook-Pro-2.local>
2018-02-11 13:46:27 +0100

    startup stuff

--- angularApp/app/shared/components/customfooter/customfooter.component.ts
+++ angularApp/app/shared/components/customfooter/customfooter.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-custom-footer',
-    templateUrl: './customfooter.component.html'
+    templateUrl: './customfooter.component.html',
+    styleUrls: ['/customfooter.component.scss']
 })
 
 export class CustomFooterComponent {
