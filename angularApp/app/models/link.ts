fe0395b67d638c9b333cf615160e21e1616cb076
Developer <dev@Administrators-MacBook-Pro-2.local>
2018-02-25 23:33:24 +0100

    rename server namespace, add title to link object

--- angularApp/app/models/link.ts
+++ angularApp/app/models/link.ts
@@ -1,16 +1,8 @@
 export class Link {
 	public id: string;
 	public name: string;
+	public title: string;
 	public desc: string;
 	public url: string;
 	public urldesc: string;
-
-	public get hostName() {
-		const match = this.url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
-		if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
-			return match[2];
-		} else {
-			return null;
-		}
-	}
 }
