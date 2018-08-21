21042dd22d0d6031c68db8a17d7236bca0cd39a5
DonCorleone <vitocorleone77@gmail.com>
2018-08-21 22:33:42 +0200

    staic files handling in client, IHostingEnvironment

--- angularApp/app/shared/utils/config.service.ts
+++ angularApp/app/shared/utils/config.service.ts
@@ -4,12 +4,18 @@ import { Injectable } from '@angular/core';
 export class ConfigService {
 
 	_apiURI: string;
+	_staticURI: string;
 
 	constructor() {
 		this._apiURI = 'https://localhost:5001/api/v1';
+		this._staticURI = 'https://localhost:5001/';
 	}
 
 	getApiURI() {
 		return this._apiURI;
+	}
+
+	getStaticURI() {
+		return this._staticURI;
 	}
 }
