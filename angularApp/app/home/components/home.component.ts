8edb39060e5644578926ea02b60682ea57bba0fe
DonCorleone <vitocorleone77@gmail.com>
2018-09-07 22:23:47 +0200

    signalR added

--- angularApp/app/home/components/home.component.ts
+++ angularApp/app/home/components/home.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HubConnection } from '@aspnet/signalr';
+import * as signalR from '@aspnet/signalr';
+import { ConfigService } from '../../shared/utils/config.service';
 
 @Component({
 	selector: 'app-home-component',
 	templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+	private _hubConnection: HubConnection | undefined;
+	public async: any;
+	message = '';
+	messages: string[] = [];
+
+	constructor(
+		private configService: ConfigService) {
+	}
+
+	public sendMessage(): void {
+		const data = `Sent: ${this.message}`;
+
+		if (this._hubConnection) {
+
+			this._hubConnection.invoke('Send', data);
+			this.messages.push(data);
+		}
+	}
+
+	ngOnInit(): void {
+		this._hubConnection = new signalR.HubConnectionBuilder()
+			.withUrl(this.configService._staticURI + 'loopy')
+			.configureLogging(signalR.LogLevel.Information)
+			.build();
+
+		this._hubConnection.start().catch(err => console.error(err.toString()));
+
+		this._hubConnection.on('Send', (data: any) => {
+			const received = `Received: ${data}`;
+			this.messages.push(received);
+		});
+	}
 }
